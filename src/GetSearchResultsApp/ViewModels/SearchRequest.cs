71901fe72e209d9baa30d602d55e29c7d1289131
Arish Labroo <arishlabroo@hotmail.com>
2016-08-24 23:07:49 -0700

    display attribute

--- src/GetSearchResultsApp/ViewModels/SearchRequest.cs
+++ src/GetSearchResultsApp/ViewModels/SearchRequest.cs
@@ -5,14 +5,18 @@ namespace GetSearchResultsApp.ViewModels
     public partial class SearchRequest
     {
         [Required]
+        [Display(Name = "Address Line")]
         public string AddressLine { get; set; }
 
         public string City { get; set; }
+
         public string State { get; set; }
 
         [DataType(DataType.PostalCode)]
+        [Display(Name="Zip Code")]
         public string ZipCode { get; set; }
 
+        [Display(Name = "Zestimate Rent")]
         public bool ZestimateRent { get; set; }
     }
 }
